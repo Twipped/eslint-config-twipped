a9ecc472bae54852e59bb0a68f9ccab05654f8b7
Jocelyn Badgley (Twipped) <joc@twipped.com>
2019-09-06 08:09:12 -0700

    Move local .eslint to .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
-{
+module.exports = exports = {
+	plugins: [ 'node' ],
+	env: {
+		node: true,
+	},
 	"extends": [
+		"eslint:recommended",
 		"./rules/best-practices.js",
-		"./rules/es6.js",
 		"./rules/node.js",
 		"./rules/style.js"
 	],
 	"rules": {
 		"node/exports-style": "off"
 	}
-}
+};
