af074996926fea1434d8cdecf30ad112e1c6ced9
Jocelyn Badgley (Twipped) <joc@twipped.com>
2022-04-17 12:38:30 -0700

    chore: Fix failing lint

--- configs/node-rollup.js
+++ configs/node-rollup.js
@@ -27,6 +27,6 @@ module.exports = {
 
     // Ensure consistent use of file extension within the import path
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    'import/extensions': 'off'
+    'import/extensions': 'off',
   },
 };
