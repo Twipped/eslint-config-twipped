fde1eaa095d428a87ba5680bd1ab2a41ef65a38a
Jocelyn Badgley (Twipped) <joc@twipped.com>
2022-09-21 21:03:23 -0700

    feat: Switch from eslint-plugin-node to eslint-plugin-n

--- rules/node.js
+++ rules/node.js
@@ -1,17 +1,14 @@
 module.exports = exports = {
-  plugins: [ 'node' ],
+  plugins: [ 'n' ],
 
   extends: [
-    'plugin:node/recommended',
+    'plugin:n/recommended',
   ],
 
   rules: {
     // disallow use of the Buffer() constructor
     // https://eslint.org/docs/rules/no-buffer-constructor
     'no-buffer-constructor': 'error',
-
-    // enforces error handling in callbacks (node environment)
-    'node/handle-callback-err': 'warn',
 
     // disallow use of new operator with the require function
     'no-new-require': 'error',
@@ -23,34 +20,37 @@ module.exports = exports = {
     // warn if something calls process.exit without explicit allow
     'no-process-exit': 'warn',
 
+    // enforces error handling in callbacks (node environment)
+    'n/handle-callback-err': 'warn',
+
     // enforce using `exports` for named exports
-    'node/exports-style': [ 'error', 'exports', { 'allowBatchAssign': true } ],
+    'n/exports-style': [ 'error', 'exports', { 'allowBatchAssign': true } ],
 
     // disallow the assignment to exports without also assigning to module.exports
     // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-exports-assign.md
-    'node/no-exports-assign': 'error',
+    'n/no-exports-assign': 'error',
 
     // ensure correct hashbang path when hashbang is used in executable scripts
     // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/shebang.md
-    'node/shebang': 'error',
+    'n/shebang': 'error',
 
     // ensure that if the package defines a bin path, that path is published with the package
     // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-process-exit.md
-    'node/no-unpublished-bin': 'error',
+    'n/no-unpublished-bin': 'error',
 
     // ensure avoidance of any apis deprecated in our current node version
     // https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-deprecated-api.md
-    'node/no-deprecated-api': 'warn',
+    'n/no-deprecated-api': 'warn',
 
-    'node/no-unsupported-features/es-syntax': 'warn',
-    'node/no-unsupported-features/es-builtins': 'warn',
-    'node/no-unsupported-features/node-builtins': 'warn',
+    'n/no-unsupported-features/es-syntax': 'warn',
+    'n/no-unsupported-features/es-builtins': 'warn',
+    'n/no-unsupported-features/node-builtins': 'warn',
 
-    'node/no-extraneous-import': 'off',
-    'node/no-extraneous-require': 'off',
-    'node/no-missing-import': 'off',
-    'node/no-missing-require': 'off',
-    'node/no-unpublished-import': 'off',
-    'node/no-unpublished-require': 'off',
+    'n/no-extraneous-import': 'off',
+    'n/no-extraneous-require': 'off',
+    'n/no-missing-import': 'off',
+    'n/no-missing-require': 'off',
+    'n/no-unpublished-import': 'off',
+    'n/no-unpublished-require': 'off',
   },
 };
